b50588a96025b4a0d3f4d8ef467ca76996ab9321
yatish1234546 <katagaryatish@gmail.com>
2021-01-01 21:56:06 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Import stylesheets
 import "./style.css";
 // import "./promise/promise.js";
-import "./higher-order-functions/higherorderfuncs.js";
+import "./prototypes/prototype-interview.js";
+// import "./higher-order-functions/higherorderfuncs.js";
 // Write Javascript code!
+
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;

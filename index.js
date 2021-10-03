69f39c70cca8b0ba0222df1284bce99033dad487
yatish1234546 <katagaryatish@gmail.com>
2021-10-03 14:15:05 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // Import stylesheets
-import "./style.css";
-import "./promise/callbacks.js";
+import './style.css';
+import './promise/callbacks.js';
 // import "./prototypes/prototype-interview.js";
 // import "./memoization/memoization.js";
 // import "./higher-order-functions/higherorderfuncs.js";
 // Write Javascript code!
 
-const appDiv = document.getElementById("app");
+const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function cache() {
+  for (let i = 0; i < 10000; i++) {
+    console.log(i);
+  }
+}
+
+cache();
